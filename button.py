d69eb2065d23da61210cbaab02c2da80b0944e89
marekk3301 <55376943+marekk3301@users.noreply.github.com>
2025-02-26 00:21:09 +0100

    Create button.py

---
+++ button.py
@@ -0,0 +1,19 @@
+import time
+import board
+import digitalio
+
+# Dioda LED
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+# Przycisk
+button = digitalio.DigitalInOut(board.GP3) # Tu trzeba podać numer pinu do którego wpinamy przycisk
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+while True:
+    if not button.value:  # Sprawdzamy czy przycisk jest wciśnięty
+        led.value = True  # Jeśli jest to włączamy led
+    else:
+        led.value = False  # Jeśli nie to wyłączamy
+    time.sleep(0.1)  # zatrzymujemy się na sekundę żeby przycisk lepiej działał
